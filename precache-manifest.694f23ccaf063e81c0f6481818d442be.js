4a94662017d748e65920da1827858401715f10b8
Vilhelm Prytz (via Travis CI) <vilhelm@prytznet.se>
2020-10-19 14:43:15 +0000

    Deploy wilfredproject.org to github.com/wilfred-dev/wilfredproject.github.io.git:master

---
+++ precache-manifest.694f23ccaf063e81c0f6481818d442be.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c6a244bcda287be876f32b95c70e19f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc071f2131ff19651b3b",
+    "url": "/static/css/main.b4b8b48a.chunk.css"
+  },
+  {
+    "revision": "3a037628a7bd2bed3256",
+    "url": "/static/js/2.c90fb99c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c90fb99c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc071f2131ff19651b3b",
+    "url": "/static/js/main.4cdc38bb.chunk.js"
+  },
+  {
+    "revision": "ce9f3165c21c03b491cb",
+    "url": "/static/js/runtime-main.6c90a4e7.js"
+  },
+  {
+    "revision": "dd958e1fb74cb703603bca446cc739df",
+    "url": "/static/media/discord.dd958e1f.svg"
+  },
+  {
+    "revision": "40874bb36923f35bad4b2d9de922556e",
+    "url": "/static/media/discourse.40874bb3.svg"
+  },
+  {
+    "revision": "07883e93734b98cae0f7b9c55d287250",
+    "url": "/static/media/github.07883e93.svg"
+  },
+  {
+    "revision": "07f5dc9674a6787cc78c17a18370dc68",
+    "url": "/static/media/logo.07f5dc96.svg"
+  }
+]);
